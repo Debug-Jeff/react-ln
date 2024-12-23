8b01410ea782a17a817f9a702f6ab061bb4690e8
Jeff <jmutugi00.0@gmail.com>
2024-12-23 14:44:17 +0300

    More changes

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,15 +2,22 @@
 //There is no need of importing the fragment tag from react.
 
 function ListGroup() {
-  const items = ["New York", "Los Angeles", "Tokyo", "Paris"];
+  let items = ["New York", "Los Angeles", "Tokyo", "Paris"];
+  items = [];
 
-  
+    // const message = items.length === 0 ? <p>No item found</p> : null;;
+
+    const getMessage = () => {
+        return items.length === 0 ? <p>No item found</p> : null;;
+    }
+
   return (
     //This is one way to write the code quickly with the Fragment tag
     //The Fragment tag is used to group elements together
     //By having an empty tag you can group elements together without having to use a div tag ; the same function as importing a fragment tag from react.
     <>
       <h1>List</h1>
+      { getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
